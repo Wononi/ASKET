a615f28cc0c95be1406fe64c9f578f7f78d6de38
Wononi <wononi02144@gmail.com>
2023-10-18 17:27:32 +0300

    работа над ошибками

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 import '../style/style.scss'
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            document.querySelector('.scroll__nav').classList.add('scroll__nav-show')
+        } else {
+            document.querySelector('.scroll__nav').classList.remove('scroll__nav-show')
+        }
+    });
+});
+
+observer.observe(document.querySelector('header nav'))
